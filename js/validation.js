27fd1a4a7d8d5ff334bf1aecf7b18b766f27add9
Randall Knickerbocker <randallknickerbocker@Randalls-MacBook-Pro.local>
2015-08-28 23:12:16 -0400

    set up input form in html

---
+++ js/validation.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+  var email="";
+
+  $("#submitBtn").click( function(){
+       console.log("CLICK WORKS");
+  
+ 
+
+     email=$("#email").val();
+
+
+     validate("email",email );
+   
+
+  });
+
+  function validate(dataType, data){
+
+     console.log( dataType );
+     console.log( data );
+
+     if( data != ""){
+     	
+     }
+
+
+
+
+  }
+
+});
